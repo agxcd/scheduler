77911c2a972386c7878f91ae8a5b0d4bf1bd52fd
agxcd <agxcdcode@gmail.com>
2020-12-08 09:27:20 -0800

    add Header, Empty to Appointment section and storybook

--- stories/index.js
+++ stories/index.js
@@ -130,3 +130,17 @@ storiesOf("InterviewerList", module)
       onChange={action("setInterviewer")}
     />
   ));
+
+//  Appointments
+import Appointment from "components/Appointment/index.js";
+import Header from "components/Appointment/Header.js";
+import Empty from "components/Appointment/Empty.js";
+
+storiesOf("Appointment", module)
+  .addParameters({
+    backgrounds: [{ name: "white", value: "#fff", default: true }],
+  })
+  .add("Appointment", () => <Appointment />)
+  .add("Appointment with Time", () => <Appointment time="12pm" />)
+  .add("Header", () => <Header time="12pm" />)
+  .add("Empty", () => <Empty onAdd={action("onAdd")} />);
