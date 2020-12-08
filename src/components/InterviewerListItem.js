f899f653c45ead1984d7bcd82e5626a5d5928785
agxcd <agxcdcode@gmail.com>
2020-12-08 05:17:29 -0800

    InterviewerListItem.js -cleanup

--- src/components/InterviewerListItem.js
+++ src/components/InterviewerListItem.js
@@ -3,16 +3,6 @@ import classNames from "classnames/bind";
 
 import "components/InterviewerListItem.scss";
 export default function InterviewerListItem(props) {
-  //   const DayListClass = classNames("day-list__item", {
-  //     "day-list__item--selected": props.selected,
-  //     "day-list__item--full": props.spots === 0,
-  //   });
-  //   return (
-  //     <li className={DayListClass} onClick={() => props.setDay(props.name)}>
-  //       <h2 className="text--regular">{props.name}</h2>
-  //       <h3 className="text--light">{formatSpots(props.spots)}</h3>
-  //     </li>
-  //   );
   const InterviewerItemClass = classNames("interviewers__item", {
     "interviewers__item--selected": props.selected,
   });
