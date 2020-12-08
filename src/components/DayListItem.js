2ed651cca68df3b180a09ec39c3aa2fbcdb603f2
agxcd <agxcdcode@gmail.com>
2020-12-08 02:56:06 -0800

    Styling the DayListItem storybook

--- src/components/DayListItem.js
+++ src/components/DayListItem.js
@@ -1,10 +1,32 @@
 import React from "react";
+import classNames from "classnames/bind";
+
+import "components/DayListItem.scss";
 
 export default function DayListItem(props) {
+  const DayListClass = classNames("day-list__item", {
+    "day-list__item--selected": props.selected,
+    "day-list__item--full": props.spots === 0,
+  });
   return (
-    <li onClick={() => props.setDay(props.name)}>
+    <li className={DayListClass} onClick={() => props.setDay(props.name)}>
       <h2 className="text--regular">{props.name}</h2>
       <h3 className="text--light">{props.spots} spots remaining</h3>
     </li>
   );
 }
+
+//   const buttonClass = classNames("button", {
+//     "button--confirm": props.confirm,
+//     "button--danger": props.danger,
+//   });
+
+//   return (
+//     <button
+//       className={buttonClass}
+//       onClick={props.onClick}
+//       disabled={props.disabled}
+//     >
+//       {props.children}
+//     </button>
+//   );
