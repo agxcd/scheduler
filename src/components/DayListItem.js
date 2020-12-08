47de6d15d080acae9b4d3204b3cfd3e0e6780132
agxcd <agxcdcode@gmail.com>
2020-12-08 03:23:42 -0800

    DayListItem test passed

--- src/components/DayListItem.js
+++ src/components/DayListItem.js
@@ -4,6 +4,17 @@ import classNames from "classnames/bind";
 import "components/DayListItem.scss";
 
 export default function DayListItem(props) {
+  const formatSpots = function (spots) {
+    if (spots === 0) {
+      return "no spots remaining";
+    }
+    if (spots === 1) {
+      return "1 spot remaining";
+    } else {
+      return `${spots} spots remaining`;
+    }
+  };
+
   const DayListClass = classNames("day-list__item", {
     "day-list__item--selected": props.selected,
     "day-list__item--full": props.spots === 0,
@@ -11,22 +22,7 @@ export default function DayListItem(props) {
   return (
     <li className={DayListClass} onClick={() => props.setDay(props.name)}>
       <h2 className="text--regular">{props.name}</h2>
-      <h3 className="text--light">{props.spots} spots remaining</h3>
+      <h3 className="text--light">{formatSpots(props.spots)}</h3>
     </li>
   );
 }
-
-//   const buttonClass = classNames("button", {
-//     "button--confirm": props.confirm,
-//     "button--danger": props.danger,
-//   });
-
-//   return (
-//     <button
-//       className={buttonClass}
-//       onClick={props.onClick}
-//       disabled={props.disabled}
-//     >
-//       {props.children}
-//     </button>
-//   );
