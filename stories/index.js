485f239ca2c8411fce1391cfb1d746453750c535
agxcd <agxcdcode@gmail.com>
2020-12-08 03:49:30 -0800

    DayList

--- stories/index.js
+++ stories/index.js
@@ -23,6 +23,7 @@ storiesOf("Button", module)
     </Button>
   ));
 
+// DayListItem.js
 import DayListItem from "components/DayListItem";
 storiesOf("DayListItem", module)
   .addParameters({
@@ -33,4 +34,35 @@ storiesOf("DayListItem", module)
   .add("Full", () => <DayListItem name="Monday" spots={0} />)
   .add("Clickable", () => (
     <DayListItem name="Tuesday" setDay={action("setDay")} spots={5} />
+  ));
+
+// DayList.js
+import DayList from "components/DayList";
+const days = [
+  {
+    id: 1,
+    name: "Monday",
+    spots: 2,
+  },
+  {
+    id: 2,
+    name: "Tuesday",
+    spots: 5,
+  },
+  {
+    id: 3,
+    name: "Wednesday",
+    spots: 0,
+  },
+];
+
+storiesOf("DayList", module)
+  .addParameters({
+    backgrounds: [{ name: "dark", value: "#222f3e", default: true }],
+  })
+  .add("Monday", () => (
+    <DayList days={days} day={"Monday"} setDay={action("setDay")} />
+  ))
+  .add("Tuesday", () => (
+    <DayList days={days} day={"Tuesday"} setDay={action("setDay")} />
   ));
