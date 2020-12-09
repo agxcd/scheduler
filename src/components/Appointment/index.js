2906564e43dfef525a7ba69ad59a628441a503db
agxcd <agxcdcode@gmail.com>
2020-12-09 00:43:27 -0800

    display appointment, conditional render

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -10,15 +10,25 @@ import Error from "./Error";
 import Form from "./Form";
 
 export default function Appointment(props) {
+  let status;
+  if (!props.interview) {
+    status = <Empty />;
+  } else {
+    status = (
+      <Show
+        student={props.interview.student}
+        interviewer={props.interview.interviewer.name}
+      />
+    );
+  }
   return (
     <article className="appointment">
       <Header time={props.time} />
-      <Empty />
-      <Show />
-      <Confirm />
+      {status}
+      {/* <Confirm />
       <Status />
       <Error />
-      <Form />
+      <Form /> */}
     </article>
   );
 }
