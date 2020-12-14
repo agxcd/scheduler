0dd8ec03ab3a463e548474f01b5efe24150af417
agxcd <agxcdcode@gmail.com>
2020-12-14 14:04:53 -0800

    Editing Appointments // still need to fix axios

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -17,6 +17,7 @@ const CREATE = "CREATE";
 const SAVING = "SAVING";
 const CONFIRM = "CONFIRM";
 const DELETING = "DELETING";
+const EDIT = "EDIT";
 
 export default function Appointment(props) {
   const { mode, transition, back } = useVisualMode(
@@ -48,6 +49,7 @@ export default function Appointment(props) {
           student={props.interview.student}
           interviewer={props.interview.interviewer}
           onDelete={() => transition(CONFIRM)}
+          onEdit={() => transition(EDIT)}
         />
       )}
       {mode === CREATE && (
@@ -65,6 +67,14 @@ export default function Appointment(props) {
           message="Are you sure you would like to delete?"
           onCancel={() => back()}
           onConfirm={deleteInterview}
+        />
+      )}
+      {mode === EDIT && (
+        <Form
+          interviewers={props.interviewers}
+          bookInterview={props.bookInterview}
+          onSave={save}
+          onCancel={() => back()}
         />
       )}
     </article>
